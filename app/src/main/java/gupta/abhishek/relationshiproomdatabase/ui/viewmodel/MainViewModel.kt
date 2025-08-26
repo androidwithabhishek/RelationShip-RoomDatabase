4faacfe8bd3e4e1844e59feeb40e8491fc6f7087
abhishekgupta <abhishekgupta766853@gmail.com>
2025-08-27 00:14:04 +0530

    fads

--- app/src/main/java/gupta/abhishek/relationshiproomdatabase/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/gupta/abhishek/relationshiproomdatabase/ui/viewmodel/MainViewModel.kt
@@ -4,17 +4,26 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import gaur.himanshu.roomdatabaserelations.common.OneToOneRelation
 import gaur.himanshu.roomdatabaserelations.database.ExampleDao
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val exampleDao: ExampleDao) : ViewModel() {
 
-    
 
+    val list = mutableStateOf(emptyList<String>())
 
-   
+    init {
+
+        viewModelScope.launch(Dispatchers.IO) {
+
+            async { exampleDao.insertOwner(OneToOneRelation.getOwnerList()) }
+        }
+    }
+
 
 }
